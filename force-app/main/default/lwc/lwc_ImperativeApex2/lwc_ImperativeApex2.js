cdf1073a500e3e0263bfb505d1b70d8b7b43ac95
KVNSKumar <105564856+KVNSKumar@users.noreply.github.com>
2022-06-14 23:26:59 +0530

    initial Commit

---
+++ force-app/main/default/lwc/lwc_ImperativeApex2/lwc_ImperativeApex2.js
@@ -0,0 +1,11 @@
+import { LightningElement } from 'lwc';
+
+import Accountlist from '@salesforce/apex/lwc_getaccountlist.getAccountList';
+
+export default class Lwc_ImperativeApex2 extends LightningElement {
+result;
+error;
+   
+  
+   
+}
